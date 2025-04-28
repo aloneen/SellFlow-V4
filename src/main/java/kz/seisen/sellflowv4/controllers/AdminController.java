a17507c30b49ade971a8c6cc17edb10901b737ea
aloneen <seisenbek2005@mail.ru>
2025-04-28 23:34:54 +0500

    Fixed problem with my own account in Admin Panel

--- src/main/java/kz/seisen/sellflowv4/controllers/AdminController.java
+++ src/main/java/kz/seisen/sellflowv4/controllers/AdminController.java
@@ -5,6 +5,7 @@ import kz.seisen.sellflowv4.entities.SupportMessage;
 import kz.seisen.sellflowv4.entities.User;
 import kz.seisen.sellflowv4.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +19,11 @@ public class AdminController {
     private AdminService adminService;
 
     @GetMapping("/users")
-    public String listUsers(Model model) {
+    public String listUsers(Model model, Authentication authentication) {
         List<User> users = adminService.listAllUsers();
+        String currentUsername = authentication.getName();
         model.addAttribute("users", users);
+        model.addAttribute("currentUsername", currentUsername);
         return "admin/users";    // new template
     }
 
